2fc01467d04a698b624390ac913a7de3f2d0c38f
Rodrigo Silveira <rsilveira@deseretdigital.com>
2014-04-18 09:41:03 -0600

    Added mock composer file

---
+++ composer.js
@@ -0,0 +1,29 @@
+{
+    "name": "bigbrotherjs",
+    "type": "library",
+    "description": "User monitoring",
+    "keywords": [
+        "behaviors",
+        "analytics"
+    ],
+    "homepage": "http://bigbrotherjs.com/",
+    "license": "GPL",
+    "authors": [
+        {
+            "name": "Rodrigo Silveira",
+            "email": "webmaster@rodrigo-silveira.com"
+        }
+    ],
+    "support": {
+        "email": "webmaster@rodrigo-silveira.com",
+        "wiki": "https://github.com/",
+        "issues": "https://github.com/",
+        "source": "https://github.com/"
+    },
+    "require": {
+        "php": ">=5.3.2"
+    },
+    "autoload": {
+        "psr-0": { "BB\\": "lib/" }
+    }
+}
